d88382fdfb81992e7eea92cc79e97ead1b41fe73
Pranav <pranavtaukari@gmail.com>
2023-07-18 01:46:17 -0700

    Modified App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
 } from "@aws-amplify/ui-react";
 import ChatApp from './components/chatapp';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
+import './App.css'
+
 Amplify.configure(awsconfig);
 
 const App = () => {
   return (
     <>
-    <div>
-      <h1>Welcome to the Chat App</h1>
-      < ChatApp />
+    <div className="container">
+      <h1 className="heading">TAMU Virtual TA</h1>
+      <ChatApp />
     </div>
     </>
   );
