8e6c82a07ef06cfda92be113dcedbe2833015da6
Pranav <pranavtaukari@gmail.com>
2023-07-18 01:25:43 -0700

    Updated API key

--- src/components/chatapp.js
+++ src/components/chatapp.js
@@ -6,7 +6,7 @@ import user from './assets/user.svg';
 import send from './assets/send.svg';
 import './chatapp.css';
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 function ChatApp() {
   const [chatMessages, setChatMessages] = useState([]);
