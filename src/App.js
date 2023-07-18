53d8365cd4eeaa8c28596839c8f3bf24ee6490cf
Pranav <pranavtaukari@gmail.com>
2023-07-18 02:52:52 -0700

    Added navbar and signout button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+// import "@aws-amplify/ui-react/styles.css";
+// import {
+//   withAuthenticator,
+// } from "@aws-amplify/ui-react";
+// import ChatApp from './components/chatapp';
+// import { Amplify } from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import './App.css'
+
+// Amplify.configure(awsconfig);
+
+// const App = () => {
+//   return (
+//     <>
+//     <div className="container">
+//       <h1 className="heading">TAMU Virtual TA</h1>
+//       <ChatApp />
+//     </div>
+//     </>
+//   );
+// };
+
+// export default withAuthenticator(App, { hideSignUp: true });
+
+import { Amplify, Auth } from "aws-amplify"; // Import Auth from aws-amplify
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import ChatApp from './components/chatapp';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import './App.css'
+import awsExports from "./aws-exports";
+import "./App.css";
+import ChatApp from "./components/chatapp";
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsExports);
 
-const App = () => {
+function App({ user }) {
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("Error while signing out!", error);
+    }
+  };
+
   return (
     <>
-    <div className="container">
-      <h1 className="heading">TAMU Virtual TA</h1>
-      <ChatApp />
-    </div>
+      <div className="navbar">
+        <h1 className="heading">TAMU Virtual TA</h1>
+        <button onClick={handleSignOut}>Sign out</button>
+      </div>
+      <div className="container">
+        <h1>Hello {user.attributes.email}</h1>
+        <ChatApp/>
+      </div>
     </>
   );
-};
+}
 
 export default withAuthenticator(App, { hideSignUp: true });
