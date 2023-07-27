3021df01eb2aa2b6174d755fa092bd505e85c033
Pranav <pranavtaukari@gmail.com>
2023-07-27 15:37:10 -0700

    Context feature/Login timeout/Profile feature

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@
 
 // export default withAuthenticator(App, { hideSignUp: true });
 
+//--------------------working as of 25/7------------------------------------- 
 import { Amplify, Auth } from "aws-amplify"; // Import Auth from aws-amplify
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
@@ -48,10 +49,47 @@ function App({ user }) {
       </div>
       <div className="container">
         <h1>Hello {user.attributes.email}</h1>
-        <ChatApp/>
+        <ChatApp userProfile={user.attributes.profile}/>
       </div>
     </>
   );
 }
 
 export default withAuthenticator(App, { hideSignUp: true });
+
+
+
+// import { Amplify, Auth } from "aws-amplify"; // Import Auth from aws-amplify
+// import { withAuthenticator } from "@aws-amplify/ui-react";
+// import "@aws-amplify/ui-react/styles.css";
+// import awsExports from "./aws-exports";
+// import "./App.css";
+// import ChatApp from "./components/chatapp";
+
+// Amplify.configure(awsExports);
+
+// function App({ user }) {
+//   const handleSignOut = async () => {
+//     try {
+//       await Auth.signOut();
+//     } catch (error) {
+//       console.log("Error while signing out!", error);
+//     }
+//   };
+
+//   return (
+//     <div style={{ minHeight: "100vh" }}> {/* Wrap content in a div with min-height */}
+//       <div className="navbar">
+//         <h1 className="heading">TAMU Virtual TA</h1>
+//         <button onClick={handleSignOut}>Sign out</button>
+//       </div>
+//       <div className="container">
+//         <h1>Hello {user.attributes.profile}</h1>
+//         <ChatApp userProfile={user.attributes.profile} />
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// export default withAuthenticator(App, { hideSignUp: true });
--- src/components/chatapp.js
+++ src/components/chatapp.js
@@ -1,3 +1,144 @@
+// import { useState, useRef, useEffect } from 'react';
+// import { Configuration, OpenAIApi } from 'openai'; // Import OpenAI classes
+
+// import bot from './assets/bot.svg';
+// import user from './assets/user.svg';
+// import send from './assets/send.svg';
+// import './chatapp.css';
+
+// const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+// function ChatApp() {
+//   const [chatMessages, setChatMessages] = useState([]);
+//   const promptInputRef = useRef(null);
+//   const chatContainerRef = useRef(null);
+
+//   useEffect(() => {
+//     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+//   }, [chatMessages]);
+
+//   async function fetchChatCompletion(prompt) {
+//     const configuration = new Configuration({ apiKey: OPENAI_API_KEY });
+//     const openai = new OpenAIApi(configuration);
+
+//     const completion = await openai.createChatCompletion({
+//       model: 'gpt-3.5-turbo',
+//       messages: [
+//         { role: 'system', content: 'You are a helpful assistant.' },
+//         { role: 'user', content: prompt },
+//       ],
+//     });
+
+//   return completion.data.choices[0].message.content;
+//   }
+
+//   function loader(elementRef) {
+//     const element = elementRef.current;
+//     if (!element) return;
+
+//     element.textContent = '';
+
+//     const interval = setInterval(() => {
+//       setChatMessages((prevMessages) => {
+//         const lastMessage = prevMessages[prevMessages.length - 1];
+//         const loadingDots = lastMessage.value + '.';
+//         const updatedMessage = { ...lastMessage, value: loadingDots };
+//         return [...prevMessages.slice(0, -1), updatedMessage];
+//       });
+//     }, 300);
+
+//     return () => clearInterval(interval);
+//   }
+
+//   function typeText(element, text) {
+//     let index = 0;
+
+//     const interval = setInterval(() => {
+//       if (index < text.length) {
+//         setChatMessages((prevMessages) => {
+//           const lastMessage = prevMessages[prevMessages.length - 1];
+//           const updatedMessage = { ...lastMessage, value: lastMessage.value + text.charAt(index) };
+//           return [...prevMessages.slice(0, -1), updatedMessage];
+//         });
+//         index++;
+//       } else {
+//         clearInterval(interval);
+//       }
+//     }, 20);
+//   }
+// const handleSubmit = async (e) => {
+//     e.preventDefault();
+  
+//     const prompt = promptInputRef.current.value;
+//     promptInputRef.current.value = '';
+  
+//     const prompt2 = `
+//       If they provide code, explain what is wrong with it.  Do not provide new or corrected code,
+//       just explain in words what's wrong with the code provided.  Do not solve the problem
+//       for the student.
+    
+//       If they ask a question about Python, go ahead and answer it generally, but do not provide code as part of your answer.
+//       code: \`\`\`${prompt}\`\`\`
+//     `;
+  
+//     setChatMessages((prevMessages) => [
+//       ...prevMessages,
+//       { isAi: false, value: prompt },
+//       { isAi: true, value: '...' }, // Add the loading indicator
+//     ]);
+  
+//     const lastMessageIndex = chatMessages.length;
+//     const messageDiv = document.getElementById(`message-${lastMessageIndex}`);
+//     if (messageDiv) loader(messageDiv);
+  
+//     try {
+//       const completion = await fetchChatCompletion(prompt2);
+//       setChatMessages((prevMessages) => [
+//         ...prevMessages.slice(0, -1),
+//         { isAi: true, value: completion },
+//       ]);
+//       console.log('Updated chatMessages:', chatMessages);
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
+  
+
+//   return (
+//     <div id="app">
+//       <div id="chat_container" ref={chatContainerRef}>
+//         {chatMessages.map((message, index) => (
+//           <div className={`wrapper ${message.isAi? 'ai' : ''}`} key={index}>
+//             <div className="chat">
+//               <div className="profile">
+//                 <img src={message.isAi ? bot : user} alt={message.isAi ? 'bot' : 'user'} />
+//               </div>
+//               <div className="message" id={`message-${index}`}>
+//                 {message.value}
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       <form onSubmit={handleSubmit}>
+//         <textarea
+//           name="prompt"
+//           rows="1"
+//           cols="1"
+//           placeholder="Ask Virtual TA..."
+//           ref={promptInputRef}
+//         ></textarea>
+//         <button type="submit">
+//           <img src={send} alt="send" />
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default ChatApp;
+
 import { useState, useRef, useEffect } from 'react';
 import { Configuration, OpenAIApi } from 'openai'; // Import OpenAI classes
 
@@ -8,8 +149,9 @@ import './chatapp.css';
 
 const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
-function ChatApp() {
+function ChatApp({ userProfile }) {
   const [chatMessages, setChatMessages] = useState([]);
+  const [userPrompts, setUserPrompts] = useState([]);
   const promptInputRef = useRef(null);
   const chatContainerRef = useRef(null);
 
@@ -17,33 +159,24 @@ function ChatApp() {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
   }, [chatMessages]);
 
-  async function fetchChatCompletion(prompt) {
+  async function fetchChatCompletion(message, context) {
     const configuration = new Configuration({ apiKey: OPENAI_API_KEY });
     const openai = new OpenAIApi(configuration);
-
+    console.log(context);
+  
+    const messages = [
+      { role: 'system', content: context },
+      { role: 'user', content: message },
+    ];
+  
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a helpful assistant.' },
-        // {
-        //   role: 'user',
-        //   content: 'If they provide code, explain what is wrong with it.  Do not provide new or corrected code, just explain in words what\'s wrong with the code provided.Do not solve the problem for the student.',
-        // },
-        // { role: 'user', content: 'If they ask a question about python, go ahead and answer it generally, but do not provide code as part of your answer.' },
-        { role: 'user', content: prompt },
-      ],
+      messages: messages,
     });
-
-  return completion.data.choices[0].message.content;
-  //   let response = completion.data.choices[0].message.content;
-  //   console.log(response);
-
-  // // Remove code snippets from the response
-  //   response = response.replace(/`[^`]+`/g, '');
-
-  // return response.trim();
+  
+    return completion.data.choices[0].message.content;
   }
-
+  
   function loader(elementRef) {
     const element = elementRef.current;
     if (!element) return;
@@ -78,56 +211,39 @@ function ChatApp() {
       }
     }, 20);
   }
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
+  const userProfileOptions = {
+    1: `
+    If they provide code, explain what is wrong with it.  Do not provide new or corrected code,
+    just explain in words what's wrong with the code provided.  Do not solve the problem
+    for the student.
   
-//     const prompt = promptInputRef.current.value;
-//     promptInputRef.current.value = '';
+    If they ask a question about Python, go ahead and answer it generally, but do not provide code as part of your answer.
+    code:`,
+    2: 'You are a helpful thermodynamics teacher',
+    3: 'User 3'
+    // Add more profiles and their corresponding prompt2 options if needed
+  };
   
-//     const prompt2 = `
-//   If they provide code, explain what is wrong with it.  Do not provide new or corrected code,
-//   just explain in words what's wrong with the code provided.  Do not solve the problem
-//   for the student.
-  
-//   If they ask a question about Python, go ahead and answer it generally, but do not provide code as part of your answer.
-//   code: \`\`\`${prompt}\`\`\`
-//   `;
-  
-//     setChatMessages((prevMessages) => [
-//       ...prevMessages,
-//       { isAi: false, value: prompt },
-//       { isAi: true, value: ' ' },
-//     ]);
-  
-//     const lastMessageIndex = chatMessages.length;
-//     const messageDiv = document.getElementById(`message-${lastMessageIndex}`);
-//     if (messageDiv) loader(messageDiv);
-  
-//     try {
-//       const completion = await fetchChatCompletion(prompt2);
-//       setChatMessages((prevMessages) => [
-//         ...prevMessages.slice(0, -1),
-//         { isAi: true, value: completion },
-//       ]);
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
 const handleSubmit = async (e) => {
     e.preventDefault();
   
     const prompt = promptInputRef.current.value;
     promptInputRef.current.value = '';
-  
-    const prompt2 = `
-      If they provide code, explain what is wrong with it.  Do not provide new or corrected code,
-      just explain in words what's wrong with the code provided.  Do not solve the problem
-      for the student.
+
+    //const promptTest = userProfileOptions[userProfile];
+    const promptTest = `
+    ${userProfileOptions[userProfile]}
+     \`\`\`${prompt}\`\`\``;
+    //console.log(promptTest);
     
-      If they ask a question about Python, go ahead and answer it generally, but do not provide code as part of your answer.
-      code: \`\`\`${prompt}\`\`\`
-    `;
+    // const prompt2 = `
+    //   If they provide code, explain what is wrong with it.  Do not provide new or corrected code,
+    //   just explain in words what's wrong with the code provided.  Do not solve the problem
+    //   for the student.
+    
+    //   If they ask a question about Python, go ahead and answer it generally, but do not provide code as part of your answer.
+    //   code: \`\`\`${prompt}\`\`\`
+    // `;
   
     setChatMessages((prevMessages) => [
       ...prevMessages,
@@ -138,13 +254,20 @@ const handleSubmit = async (e) => {
     const lastMessageIndex = chatMessages.length;
     const messageDiv = document.getElementById(`message-${lastMessageIndex}`);
     if (messageDiv) loader(messageDiv);
+
+    setUserPrompts((prevUserPrompts) => [...prevUserPrompts, prompt]);
+    //const context = userPrompts.slice(0, -1).join('\n');
+    const context = userPrompts.join('\n');
+    
+
   
     try {
-      const completion = await fetchChatCompletion(prompt2);
+      const completion = await fetchChatCompletion(promptTest,context); // Pass the entire chatMessages array
       setChatMessages((prevMessages) => [
         ...prevMessages.slice(0, -1),
         { isAi: true, value: completion },
       ]);
+
     } catch (error) {
       console.error('Error:', error);
     }
